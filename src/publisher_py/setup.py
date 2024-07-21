7545b6894a5939230381b6fd2afa4b397f5f6499
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-07-21 16:47:46 -0400

    Updates

--- src/publisher_py/setup.py
+++ src/publisher_py/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-                'talker = publsiher_py.publisher:main',
+                'talker = publisher_py.publisher:main',
         ],
     },
 )
