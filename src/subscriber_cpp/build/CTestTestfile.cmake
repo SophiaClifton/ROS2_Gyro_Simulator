b6ce7f58230a99b257f53e7f51ebeaee4b7a7e3d
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-07-21 16:42:24 -0400

    UPdate CmakeList and package.xml

---
+++ src/subscriber_cpp/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sophia/Documents/GitHub/ROS2_PubSub/src/subscriber_cpp
+# Build directory: /home/sophia/Documents/GitHub/ROS2_PubSub/src/subscriber_cpp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
