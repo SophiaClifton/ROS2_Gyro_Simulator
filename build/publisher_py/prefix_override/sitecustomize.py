e1066b57b7927e44f03325c4bc52155f9e4c8012
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-07-21 19:22:17 -0400

    Created .gitignore to exclude build, install and log directories

--- build/publisher_py/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sophia/Documents/GitHub/ROS2_PubSub/install/publisher_py'
--- build/subscriber_cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sophia/Documents/GitHub/ROS2_PubSub/src/subscriber_cpp
-# Build directory: /home/sophia/Documents/GitHub/ROS2_PubSub/build/subscriber_cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
