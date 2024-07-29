fa4e9b2672a54af7e01bc21578ba303b05549573
Sophia Clifton-Harting <sophiashoco@gmail.com>
2024-07-29 00:50:35 -0400

    Accounted for invalid inputs, and publisher now published randomly generated coordinates every 10 seconds

--- src/publisher_py/publisher_py/publisher.py
+++ src/publisher_py/publisher_py/publisher.py
@@ -1,32 +1,59 @@
 import rclpy
 from geometry_msgs.msg import Quaternion
+import random
+import time
 
 def main():
     rclpy.init() 
     node = rclpy.create_node("publishing_node")
     publisher = node.create_publisher(Quaternion, "publishing_topic", 10)
 
-    try:
-        while rclpy.ok():  # Keep asking as long as this node is running
-            x = input("What's x? ")
-            y = input("What's y? ")
-            z = input("What's z? ")
-            w = input("What's w? ")
+    while rclpy.ok():  # Keep asking as long as this node is running
+        try:    
+            x = random.uniform(-1, 1) #generate random coordinates rounded to 2 decimal places
+            y = random.uniform(-1, 1)
+            z = random.uniform(-1, 1)
+            w = random.uniform(-1, 1)
 
             coordinates = Quaternion()
-            coordinates.x = float(x)
-            coordinates.y = float(y)
-            coordinates.z = float(z)
-            coordinates.w = float(w)
-
-            print(coordinates) # for visualization
+            coordinates.x = round(float(x), 2)
+            coordinates.y = round(float(y), 2)
+            coordinates.z = round(float(z), 2)
+            coordinates.w = round(float(w), 2)
+            print(coordinates) 
             publisher.publish(coordinates)
+            time.sleep (10) # to avoid crashing my pc
 
+        except KeyboardInterrupt: # program will shut done properly when Ctrl+C is pressed to terminate the program
+            pass
+    node.destroy_node()
+    rclpy.shutdown()
+
+''' #for manual input
+    try:
+        while rclpy.ok():  # Keep asking as long as this node is running
+            try: #pccount for invalid input
+                x = input("What's x? ")
+                y = input("What's y? ")
+                z = input("What's z? ")
+                w = input("What's w? ")
+
+                coordinates = Quaternion()
+                coordinates.x = float(x)
+                coordinates.y = float(y)
+                coordinates.z = float(z)
+                coordinates.w = float(w)
+
+                print(coordinates) 
+                publisher.publish(coordinates)
+            except ValueError:
+                print("Invalid input. Please enter numeric values.")
     except KeyboardInterrupt: # program will shut done properly when Ctrl+C is pressed to terminate the program
         pass
 
     node.destroy_node()
     rclpy.shutdown()
+'''
 
 if __name__ == '__main__':
     main()
